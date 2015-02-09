bb9786e0a6267078019d0272f0e76eff3fbaabbc
Aluriak <lucas.bourneuf@laposte.net>
2015-02-09 23:38:26 +0100

    basic implementation of abstract class

--- evolacc/observer/observable.py
+++ evolacc/observer/observable.py
@@ -26,7 +26,22 @@ class Observable:
     Abstract class.
     """
 # CONSTRUCTOR #################################################################
+    def __init__(self, observers=None):
+        self.observers = observers if observers is not None else set()
+
 # PUBLIC METHODS ##############################################################
+    def attach_observers(self, observers):
+        """add all observer of iterable observers to self"""
+        self.observers.extend(observers)
+
+    def notify_observers(self):
+        """update all knowed observers"""
+        [obs.update() for obs in self.observers]
+
+    def detach_observers(self, observers):
+        """retract all observers in given list of self"""
+        self.observers = set(self.observers) - set(observers)
+
 # PRIVATE METHODS #############################################################
 # PREDICATS ###################################################################
 # ACCESSORS ###################################################################
--- evolacc/observer/observer.py
+++ evolacc/observer/observer.py
@@ -25,8 +25,21 @@ class Observer:
     Observer according to Observer Pattern.
     Abstract class.
     """
+
 # CONSTRUCTOR #################################################################
+    def __init__(self, observable):
+        self.observable = observable
+
 # PUBLIC METHODS ##############################################################
+    def initialize(self):
+        return NotImplemented
+
+    def update(self):
+        return NotImplemented
+
+    def finalize(self):
+        return NotImplemented
+
 # PRIVATE METHODS #############################################################
 # PREDICATS ###################################################################
 # ACCESSORS ###################################################################
