39d14137ae5e65f437c9629648ec2a74c0f2b4bd
Aluriak <lucas.bourneuf@laposte.net>
2015-02-10 00:27:42 +0100

    implementation of action, killing action

--- evolacc/action/__init__.py
+++ evolacc/action/__init__.py
@@ -1,1 +1,1 @@
-from action import *
+from basic_actions import *
--- evolacc/action/basic_actions.py
+++ evolacc/action/basic_actions.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #########################
-#       ACTION          #
+#    BASIC ACTION       #
 #########################
 
 
 #########################
 # IMPORTS               #
 #########################
-
+from evolac.action.action import Action
 
 
 
@@ -20,16 +20,22 @@
 #########################
 # CLASS                 #
 #########################
-class Action:
+class Kill(Action):
     """
-    Action, according to Command Pattern.
-    Abstract class.
+    Action that lead to unit suppression.
     """
 # CONSTRUCTOR #################################################################
+    def __init__(self, coords):
+        """
+        coords are coordinates of unit that need to be kill/suppressed.
+        """
+        self.coords = coords
+
 # PUBLIC METHODS ##############################################################
     def execute(self, simulation):
-        """ execute action on simulation """
-        return NotImplemented
+        """ Delete unit in given simulation """
+        assert(self.coords in simulation.placer) #TODO: replace that by logging
+        del simulation.placer[self.coords]
 
 # PRIVATE METHODS #############################################################
 # PREDICATS ###################################################################
