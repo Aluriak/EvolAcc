b3720d2fff7376c6326b636a89e36d32b5f06dcf
Aluriak <lucas.bourneuf@laposte.net>
2015-02-12 22:12:42 +0100

    unit tests are prepared !

---
+++ unittests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#########################
+#       UNITESTS        #
+#########################
+
+
+#########################
+# IMPORTS               #
+#########################
+import doctest
+
+
+
+
+#########################
+# FUNCTIONS             #
+#########################
+if __name__ == '__main__':
+    #print('TESTS OF DNA COMPILER…')
+    #doctest.testfile('evolacc/dnacompiler/dnacompiler.py')
+    print('TESTS OF UNIT FACTORY…')
+    doctest.testfile('evolacc/factory/unitfactory.py')
+
+
+
+    print('END OF UNIT TESTS')
+
+
+
