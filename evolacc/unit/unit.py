1126a433155fb9a415fb258d58b9cf628c1f1494
Aluriak <lucas.bourneuf@laposte.net>
2015-02-12 21:26:53 +0100

    implementation of basic API

--- evolacc/unit/unit.py
+++ evolacc/unit/unit.py
@@ -25,10 +25,37 @@ class Unit:
     Unit of life/environment.
     Composite of Components.
     """
+
+
 # CONSTRUCTOR #################################################################
+    def __init__(self, components=None)
+        """Wait for an iterable of already initialized components
+        Initialized with zero components if not provided"""
+        self.components = set(components) if components is not None else []
+
+
 # PUBLIC METHODS ##############################################################
+    def step(self, simulation):
+        """Call step method of all components"""
+        [c.step(simulation) for c in self.components]
+
+    def add(self, component):
+        """Add given component to self. Do nothing if already present"""
+        self.components.add(component)
+
+    def rmv(self, component):
+        """Remove given component to self. Do nothing if absent"""
+        self.components.remove(component)
+
+
 # PRIVATE METHODS #############################################################
 # PREDICATS ###################################################################
+    def is_life(self):
+        """Return True iff self have at least a component 
+        that answer True to the same question"""
+        return any((c.is_life() for c in self.components))
+
+    
 # ACCESSORS ###################################################################
 # CONVERSION ##################################################################
 # OPERATORS ###################################################################
