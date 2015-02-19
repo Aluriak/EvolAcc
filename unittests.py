6c9721e391d17d3a98b3ffbe8c4dd809051f6f17
Aluriak <lucas.bourneuf@laposte.net>
2015-02-19 01:29:43 +0100

    one day, we will do more unit tests

--- unittests.py
+++ unittests.py
@@ -19,6 +19,9 @@ if __name__ == '__main__':
     #print('TESTS OF DNA COMPILER…')
     #doctest.testfile('evolacc/dnacompiler/dnacompiler.py')
 
+    print('TESTS OF CONFIG…')
+    doctest.testfile('evolacc/config/config.py')
+
     print('TESTS OF UNIT FACTORY…')
     doctest.testfile('evolacc/factory/unitfactory.py')
 
