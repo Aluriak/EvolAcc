d2a2b3ab213bbd52a4db71e9ec4aac312dae8d3a
Aluriak <lucas.bourneuf@laposte.net>
2015-02-20 11:08:08 +0100

    no call needed for init logging

--- evolacc/config/conflog.py
+++ evolacc/config/conflog.py
@@ -15,32 +15,32 @@ FILENAME_LOG = 'data/outputs/evolacc.log'
 LOGGER = logging.getLogger()
 
 
-def init_logging():
-    """
-    Init logging for evolacc.
-    """
-    LOGGER.setLevel(logging.DEBUG)
-     
-    # create formatter
-    formatter = logging.Formatter(
-        '%(asctime)s :: %(levelname)s :: %(message)s'
-    )
+#########################
+# INIT LOGGING          #
+#########################
+LOGGER.setLevel(logging.DEBUG)
+ 
+# create formatter
+formatter = logging.Formatter(
+    '%(asctime)s :: %(levelname)s :: %(message)s'
+)
 
-    # create a handler to file
-    file_handler = RotatingFileHandler(
-        FILENAME_LOG, # filename
-        'a', 1000000, 1 # append, 1 Mo, 1 backup
-    )
-    # and define its level and formatter
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
+# create a handler to file
+file_handler = RotatingFileHandler(
+    FILENAME_LOG, # filename
+    'a', 1000000, 1 # append, 1 Mo, 1 backup
+)
+# and define its level and formatter
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
-    # other handler that print logs in term 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-     
-    # add handlers to LOGGER
-    for handler in (file_handler, stream_handler):
-        LOGGER.addHandler(handler)
-     
+# other handler that print logs in term 
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+ 
+# add handlers to LOGGER
+for handler in (file_handler, stream_handler):
+    LOGGER.addHandler(handler)
+ 
 
