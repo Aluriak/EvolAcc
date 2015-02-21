4deea41e5f26a0d53e75079652cd98b9c86031d9
Aluriak <lucas.bourneuf@laposte.net>
2015-02-21 12:37:27 +0100

    test all, even if they have nothing to do

--- unittests.py
+++ unittests.py
@@ -16,21 +16,14 @@ import doctest
 # FUNCTIONS             #
 #########################
 if __name__ == '__main__':
-    #print('TESTS OF DNA COMPILER…')
-    #doctest.testfile('evolacc/dnacompiler/dnacompiler.py')
-
-    print('TESTS OF CONFIG…')
+    doctest.testfile('evolacc/action/action.py')
     doctest.testfile('evolacc/config/config.py')
-
-    print('TESTS OF UNIT FACTORY…')
+    doctest.testfile('evolacc/evolacc/evolacc.py')
     doctest.testfile('evolacc/factory/unitfactory.py')
-
-    print('TESTS OF PLACING…')
+    doctest.testfile('evolacc/observer/observer.py')
     doctest.testfile('evolacc/placing/placing.py')
-
-
-
-    print('END OF UNIT TESTS')
+    doctest.testfile('evolacc/simulation/simulation.py')
+    doctest.testfile('evolacc/unit/unit.py')
 
 
 
